a17dee241c53e48c19276fd97f5ddc2a80d30063
whipm <whipm@DESKTOP-EL5JLSJ>
2022-10-29 04:52:44 +0100

    Add project files.

---
+++ Vending machine 2 electric boogaloo/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿
+namespace Vending_machine_2_electric_boogaloo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbxMars = new System.Windows.Forms.PictureBox();
+            this.pbxTwix = new System.Windows.Forms.PictureBox();
+            this.pbxDDecker = new System.Windows.Forms.PictureBox();
+            this.pbxTwirl = new System.Windows.Forms.PictureBox();
+            this.pbcFudge = new System.Windows.Forms.PictureBox();
+            this.pbxBounty = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnCheckout = new System.Windows.Forms.Button();
+            this.pbx20p = new System.Windows.Forms.PictureBox();
+            this.pbx50p = new System.Windows.Forms.PictureBox();
+            this.pbx100p = new System.Windows.Forms.PictureBox();
+            this.pbx500p = new System.Windows.Forms.PictureBox();
+            this.pbxSlot = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMars)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxTwix)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDDecker)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxTwirl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcFudge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBounty)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx20p)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx50p)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx100p)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx500p)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSlot)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxMars
+            // 
+            this.pbxMars.Image = ((System.Drawing.Image)(resources.GetObject("pbxMars.Image")));
+            this.pbxMars.Location = new System.Drawing.Point(12, 12);
+            this.pbxMars.Name = "pbxMars";
+            this.pbxMars.Size = new System.Drawing.Size(270, 130);
+            this.pbxMars.TabIndex = 0;
+            this.pbxMars.TabStop = false;
+            // 
+            // pbxTwix
+            // 
+            this.pbxTwix.Image = ((System.Drawing.Image)(resources.GetObject("pbxTwix.Image")));
+            this.pbxTwix.Location = new System.Drawing.Point(288, 12);
+            this.pbxTwix.Name = "pbxTwix";
+            this.pbxTwix.Size = new System.Drawing.Size(270, 130);
+            this.pbxTwix.TabIndex = 0;
+            this.pbxTwix.TabStop = false;
+            // 
+            // pbxDDecker
+            // 
+            this.pbxDDecker.Image = ((System.Drawing.Image)(resources.GetObject("pbxDDecker.Image")));
+            this.pbxDDecker.Location = new System.Drawing.Point(12, 150);
+            this.pbxDDecker.Name = "pbxDDecker";
+            this.pbxDDecker.Size = new System.Drawing.Size(270, 130);
+            this.pbxDDecker.TabIndex = 0;
+            this.pbxDDecker.TabStop = false;
+            // 
+            // pbxTwirl
+            // 
+            this.pbxTwirl.Image = ((System.Drawing.Image)(resources.GetObject("pbxTwirl.Image")));
+            this.pbxTwirl.Location = new System.Drawing.Point(288, 150);
+            this.pbxTwirl.Name = "pbxTwirl";
+            this.pbxTwirl.Size = new System.Drawing.Size(270, 130);
+            this.pbxTwirl.TabIndex = 0;
+            this.pbxTwirl.TabStop = false;
+            // 
+            // pbcFudge
+            // 
+            this.pbcFudge.Image = ((System.Drawing.Image)(resources.GetObject("pbcFudge.Image")));
+            this.pbcFudge.Location = new System.Drawing.Point(12, 288);
+            this.pbcFudge.Name = "pbcFudge";
+            this.pbcFudge.Size = new System.Drawing.Size(270, 130);
+            this.pbcFudge.TabIndex = 0;
+            this.pbcFudge.TabStop = false;
+            // 
+            // pbxBounty
+            // 
+            this.pbxBounty.Image = ((System.Drawing.Image)(resources.GetObject("pbxBounty.Image")));
+            this.pbxBounty.Location = new System.Drawing.Point(288, 288);
+            this.pbxBounty.Name = "pbxBounty";
+            this.pbxBounty.Size = new System.Drawing.Size(270, 130);
+            this.pbxBounty.TabIndex = 0;
+            this.pbxBounty.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 122);
+            this.richTextBox1.Multiline = false;
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox1.Size = new System.Drawing.Size(270, 20);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "£1.90";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.richTextBox2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox2.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox2.Location = new System.Drawing.Point(288, 122);
+            this.richTextBox2.Multiline = false;
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox2.Size = new System.Drawing.Size(270, 20);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "£1.70";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.richTextBox3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox3.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox3.Location = new System.Drawing.Point(12, 260);
+            this.richTextBox3.Multiline = false;
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox3.Size = new System.Drawing.Size(270, 20);
+            this.richTextBox3.TabIndex = 1;
+            this.richTextBox3.Text = "£2.10";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox4.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.richTextBox4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox4.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox4.Location = new System.Drawing.Point(288, 260);
+            this.richTextBox4.Multiline = false;
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.ReadOnly = true;
+            this.richTextBox4.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox4.Size = new System.Drawing.Size(270, 20);
+            this.richTextBox4.TabIndex = 1;
+            this.richTextBox4.Text = "£0.80";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox5.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.richTextBox5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox5.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox5.Location = new System.Drawing.Point(12, 398);
+            this.richTextBox5.Multiline = false;
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.ReadOnly = true;
+            this.richTextBox5.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox5.Size = new System.Drawing.Size(270, 20);
+            this.richTextBox5.TabIndex = 1;
+            this.richTextBox5.Text = "£0.45";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox6.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.richTextBox6.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox6.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox6.Location = new System.Drawing.Point(288, 398);
+            this.richTextBox6.Multiline = false;
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.ReadOnly = true;
+            this.richTextBox6.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox6.Size = new System.Drawing.Size(270, 20);
+            this.richTextBox6.TabIndex = 1;
+            this.richTextBox6.Text = "£0.01";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(585, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(383, 124);
+            this.listBox1.TabIndex = 2;
+            // 
+            // btnCheckout
+            // 
+            this.btnCheckout.Location = new System.Drawing.Point(585, 352);
+            this.btnCheckout.Name = "btnCheckout";
+            this.btnCheckout.Size = new System.Drawing.Size(382, 65);
+            this.btnCheckout.TabIndex = 3;
+            this.btnCheckout.Text = "Checkout";
+            this.btnCheckout.UseVisualStyleBackColor = true;
+            // 
+            // pbx20p
+            // 
+            this.pbx20p.Image = ((System.Drawing.Image)(resources.GetObject("pbx20p.Image")));
+            this.pbx20p.Location = new System.Drawing.Point(11, 451);
+            this.pbx20p.Name = "pbx20p";
+            this.pbx20p.Size = new System.Drawing.Size(120, 120);
+            this.pbx20p.TabIndex = 4;
+            this.pbx20p.TabStop = false;
+            // 
+            // pbx50p
+            // 
+            this.pbx50p.Image = ((System.Drawing.Image)(resources.GetObject("pbx50p.Image")));
+            this.pbx50p.Location = new System.Drawing.Point(137, 451);
+            this.pbx50p.Name = "pbx50p";
+            this.pbx50p.Size = new System.Drawing.Size(120, 120);
+            this.pbx50p.TabIndex = 4;
+            this.pbx50p.TabStop = false;
+            // 
+            // pbx100p
+            // 
+            this.pbx100p.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbx100p.BackgroundImage")));
+            this.pbx100p.Location = new System.Drawing.Point(263, 451);
+            this.pbx100p.Name = "pbx100p";
+            this.pbx100p.Size = new System.Drawing.Size(120, 120);
+            this.pbx100p.TabIndex = 4;
+            this.pbx100p.TabStop = false;
+            // 
+            // pbx500p
+            // 
+            this.pbx500p.Image = ((System.Drawing.Image)(resources.GetObject("pbx500p.Image")));
+            this.pbx500p.Location = new System.Drawing.Point(389, 451);
+            this.pbx500p.Name = "pbx500p";
+            this.pbx500p.Size = new System.Drawing.Size(240, 120);
+            this.pbx500p.TabIndex = 4;
+            this.pbx500p.TabStop = false;
+            // 
+            // pbxSlot
+            // 
+            this.pbxSlot.Image = ((System.Drawing.Image)(resources.GetObject("pbxSlot.Image")));
+            this.pbxSlot.Location = new System.Drawing.Point(881, 441);
+            this.pbxSlot.Name = "pbxSlot";
+            this.pbxSlot.Size = new System.Drawing.Size(63, 130);
+            this.pbxSlot.TabIndex = 5;
+            this.pbxSlot.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(995, 690);
+            this.Controls.Add(this.pbxSlot);
+            this.Controls.Add(this.pbx500p);
+            this.Controls.Add(this.pbx100p);
+            this.Controls.Add(this.pbx50p);
+            this.Controls.Add(this.pbx20p);
+            this.Controls.Add(this.btnCheckout);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pbxBounty);
+            this.Controls.Add(this.pbxTwirl);
+            this.Controls.Add(this.pbxTwix);
+            this.Controls.Add(this.pbcFudge);
+            this.Controls.Add(this.pbxDDecker);
+            this.Controls.Add(this.pbxMars);
+            this.Name = "Form1";
+            this.Text = "30035000";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMars)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxTwix)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDDecker)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxTwirl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcFudge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBounty)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx20p)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx50p)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx100p)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx500p)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSlot)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxMars;
+        private System.Windows.Forms.PictureBox pbxTwix;
+        private System.Windows.Forms.PictureBox pbxDDecker;
+        private System.Windows.Forms.PictureBox pbxTwirl;
+        private System.Windows.Forms.PictureBox pbcFudge;
+        private System.Windows.Forms.PictureBox pbxBounty;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnCheckout;
+        private System.Windows.Forms.PictureBox pbx20p;
+        private System.Windows.Forms.PictureBox pbx50p;
+        private System.Windows.Forms.PictureBox pbx100p;
+        private System.Windows.Forms.PictureBox pbx500p;
+        private System.Windows.Forms.PictureBox pbxSlot;
+    }
+}
+
